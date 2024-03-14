c66e261bc5b8e323d2d22b3dfc3eac2369f45a51
jaycelab <jayxalvarado@gmail.com>
2024-03-13 23:17:58 -0700

    created portfolio using create-react-app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Projects from "./components/pages/Projects";
+
+// JSX = JavaScript XML
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar />
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <footer>
+        <p>&copy; Jayce 2024. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
